f5a9821c48844404481a4c2cd1e5174097be598d
Atul Tiwari <atultiwari25042005@gmail.com>
2025-04-29 10:07:01 +0530

    eight

---
+++ day3.js
@@ -0,0 +1,37 @@
+// // // let num="45"
+// // // let num1=53
+// // // console.log(num+num1)
+// // // let num2=true
+// // // console.log(num2+num1)
+// // // console.log(num2+num2)
+// // // console.log(Number('4'),typeof Number('4')
+
+// // console.log('a'<'b')
+// // console.log('a'>'A')
+// // console.log('3'>'4')
+// // console.log('2'>'1')
+// // console.log('17'.charCodeAt(0))
+
+// //if ,else if ,else
+// if(1){
+//     console.log("augef")
+// }
+// else if(2){
+//     console.log("gdsiu")
+// }
+// else{
+//     console.log("efwoih")
+// }
+// // ternary operator
+// let valour = 2>1 ? 'hello' : "gagan"
+// console.log(valour)
+// 3>5?console.log("efoij"):console.log("sdifu")
+
+//array
+let arr1=[1,2,3,4]
+console.log(arr1.pop())
+console.log(arr1)
+console.log(arr1.push(5))
+console.log(arr1)
+console.log(arr1.pop())
+console.log(arr1)
--- javascript1.js
+++ javascript1.js
@@ -1,35 +1,33 @@
-let str1="Atul"
-let str2="Tiwari"
-// let str3="Hello "+str1+" "+str2
-let str3=`Hello ${str1} ${str2}`
-console.log(str3) // Hello Atul Tiwari
-// let is a reserved keyword in JS, so it cannot be used as a variable name
+// let str1="Atul"
+// let str2="Tiwari"
+// // let str3="Hello "+str1+" "+str2
+// let str3=`Hello ${str1} ${str2}`
+// console.log(str3) // Hello Atul Tiwari
+// // let is a reserved keyword in JS, so it cannot be used as a variable name
 
-// // boolean
-// let iscorrect= 4>1
-// console.log(iscorrect) // true
-// // null
-// let isnull= null
-// console.log(typeof isnull) // null
-// console.log(isnull) // null
+// // // boolean
+// // let iscorrect= 4>1
+// // console.log(iscorrect) // true
+// // // null
+// // let isnull= null
+// // console.log(typeof isnull) // null
+// // console.log(isnull) // null
 
-// // browser
-// // alert
-// alert("Hello World") // alert box
-// // confirm
-// confirm("Are you sure?") // confirm box
-// prompt
-// let age=prompt("Enter your age")
-// console.log(age) // age entered by user
-// alert(`ha bhai tum itne ${age} sal ke ho`)
-let len=str1.length
-console.log(len)
-console.log(str1.indexOf("a")) //-1
-console.log(str1.indexOf("l")) //3
-console.log(str2.indexOf("i")) //1
-console.log(str1.indexOf("tu")) //1
-console.log("Tiwari".indexOf("i")) //1
-console.log(Math.floor(2.99))
-console.log(Math.floor(Math.random()*1000))
-let num=Math.floor(Math.random()*1000)
-console.log(num)
+// // // browser
+// // // alert
+// // alert("Hello World") // alert box
+// // // confirm
+// // confirm("Are you sure?") // confirm box
+// // prompt
+// // let age=prompt("Enter your age")
+// // console.log(age) // age entered by user
+// // alert(`ha bhai tum itne ${age} sal ke ho`)
+// let len=str1.length
+// console.log(len)
+// console.log(str1.indexOf("a")) //-1
+// console.log(str1.indexOf("l")) //3
+// console.log(str2.indexOf("i")) //1
+// console.log(str1.indexOf("tu")) //1
+// console.log("Tiwari".indexOf("i")) //1
+// console.log(Math.floor(2.99))
+// console.log(Math.floor(Math.random()*900+100))
